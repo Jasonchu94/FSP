c35f104aa08dbabd743390546e551edffd057047
Jasonchu94 <Jasonchu7272@gmail.com>
2022-01-03 11:54:27 -0500

    final push before upload to heroku

---
+++ frontend/component/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const About = () => (
+
+    <div className='Footer-links'>
+        <a href='https://github.com/Jasonchu94'>Github</a>
+        &nbsp;
+        <a href='https://www.linkedin.com/in/jasonchu94/'>LinkedIn</a>
+    </div>
+)
--- frontend/component/app.jsx
+++ frontend/component/app.jsx
@@ -6,6 +6,7 @@ import LoginFormContainer from '../component/session_form/login_form_container'
 import SignupFormContainer from "../component/session_form/signup_form_container"
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import SplashContainer from './splash/splash_container';
+import { About } from './about/about';
 
 const App =() =>(
 
@@ -19,6 +20,9 @@ const App =() =>(
             <AuthRoute exact path="/login" component={LoginFormContainer}/>
             <AuthRoute exact path='/signup' component={SignupFormContainer}/>
         </Switch>
+        <footer>
+            <About/>
+        </footer>
     </div>
 
 
