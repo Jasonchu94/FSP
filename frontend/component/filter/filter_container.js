279262b6370e3e546d69d7944a3b8e5e4106c2fd
Jasonchu94 <Jasonchu7272@gmail.com>
2022-02-02 15:03:13 -0800

    setup filter

---
+++ frontend/component/filter/filter_container.js
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Filter from "./filter";
+
+const mSTP = state => ({
+
+})
+
+const mDTP = dispatch => ({
+
+})
+
+export default connect(mSTP,mDTP)(Filter)
