456298c5957a5affed5070d38d925f04336e5350
Jason Chu <jason.chu@smoothstack.com>
2022-11-18 12:40:19 -0500

    Fix links

--- frontend/component/about/about.jsx
+++ frontend/component/about/about.jsx
@@ -8,14 +8,7 @@ import React from 'react';
         <div className='footer-links'>            
                 <a target='_blank' className='links-me' href='https://github.com/Jasonchu94'>
                     <img src={window.github} id='about-images'></img>
-                </a>                
-                <a target='_blank' className='links-me' href='https://www.linkedin.com/in/jasonchu94/'>
-                    <img src={window.linkedin} id='about-images'/>
-                </a>
-                <a target='_blank' className='links-me' href='https://angel.co/u/jason-chu-20'>
-                     <img src={window.angellist} id='about-images'></img>
-                </a>
-            
+                </a>              
         </div>
     </div> 
 )
